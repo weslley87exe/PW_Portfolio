6354dc9ce530e8230b4e6b54b192125814df3c94
Weslley <pauloweslleypw8@gmail.com>
2025-11-10 19:19:14 -0300

    Ajusta main.py para deploy estável no Railway

--- Python_projects/sistema_alunos/main.py
+++ Python_projects/sistema_alunos/main.py
@@ -60,6 +60,5 @@ def api_adicionar(aluno: dict):
 
 if __name__ == "__main__":
     import uvicorn
-    port = int(os.getenv("PORT", 8000))  # pega a porta do ambiente (ex: Render)
-    uvicorn.run(app, host="0.0.0.0", port=port)
+    uvicorn.run("main:app", host="0.0.0.0", port=int(os.getenv("PORT", 8000)))
 
